99758bb72bc15fee165d9e6c9d3f054160fb31b4
sonam <chossonam39@gmail.com>
2025-09-20 18:11:13 +0530

    commiting insertionsort

---
+++ codes/src/sorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args)
+    {
+          int[] arr = {5,3,4,1,2};
+          insertion(arr);
+
+          System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr)
+    {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j=i+1;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    SelectionSort.swap(arr,j,j-1);
+
+                }
+                else{
+                    break;
+                }
+            }
+            
+        }
+    }
+}
